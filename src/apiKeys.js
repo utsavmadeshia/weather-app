b7cea711ab722b64c539c8cc8e08cca5d150b3c4
Utsav Madeshia <utsavmadeshia@gmail.com>
2023-08-13 23:32:53 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "45cc4755720f6e209a10844f64ce80b7",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
